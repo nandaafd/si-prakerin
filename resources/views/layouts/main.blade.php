06b6f019c20558fcc18678dce50820ff9fb8436b
nandaafd <nandaafd.info@gmail.com>
2022-12-29 11:01:12 +0700

    revisi tampilan

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ $title ?? 'Website' }}</title>
+    <title>{{ $title ?? 'Portal IT' }}</title>
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('/assets/css/bootstrap.css') }}">
