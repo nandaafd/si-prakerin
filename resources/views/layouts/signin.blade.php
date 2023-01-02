ef5f81eba0d0b21539710ef9b418fb8f08ba38d2
nandaafd <nandaafd.info@gmail.com>
2023-01-02 16:07:45 +0700

    migrate database

--- resources/views/layouts/signin.blade.php
+++ resources/views/layouts/signin.blade.php
@@ -5,7 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Portal IT</title>
+    
     <link rel="stylesheet" href="{{asset('css/main.css')}}">
+    <link rel="stylesheet" href="{{ asset('/assets/vendors/iconly/bold.css') }}">
     {{-- <link rel="stylesheet" href="{{asset('css/portal.css')}}"> --}}
     <link rel="stylesheet" href="{{asset('css/login.css')}}">
     <link rel="shortcut icon" href="{{asset('images/logo/logobtx.png')}}" type="image/x-icon">  
@@ -26,6 +28,14 @@
     {{-- @include('footer.footer') --}}
 </footer>
 <script src="{{ asset('/assets/js/portal.js') }}"></script>
+<script src="{{ asset('/assets/vendors/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('/assets/js/extensions/jquery.min.js') }}"></script>
+    <script src="{{ asset('/assets/vendors/perfect-scrollbar/perfect-scrollbar.min.js') }}"></script>
+    <script src="{{ asset('assets/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('/assets/js/main.js') }}"></script>
+    <script src="{{ asset('/assets/js/datetime.js') }}"></script>
+    <script src="{{ asset('/assets/vendors/simple-datatables/simple-datatables.js') }}"></script>
+    <script src="{{ asset('/assets/vendors/sweetalert2/sweetalert2.all.min.js') }}"></script>   
 @stack('js')
 </body>
 </html>
